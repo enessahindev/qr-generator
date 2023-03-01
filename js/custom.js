6ca4a9967e04beac290ee8cfbba002bfabf483ac
Enes Şahin <85828666+enessahindev@users.noreply.github.com>
2023-03-01 19:26:22 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+// nice select
+$(document).ready(function () {
+    $('select').niceSelect();
+});
+
+// generateQR
+function generateQR() {
+    document.querySelector("#qr-image").style.display = "block";
+    let QRtext = document.querySelector("#text").value;
+    if (QRtext.trim().length == 0) {
+        document.querySelector("#qr-image .error").innerHTML = "Please enter text";
+        document.querySelector("#img").style.display = "none";
+    } else {
+        document.querySelector("#img").style.display = "block";
+        document.querySelector("#qr-image .error").innerHTML = "";
+        document.querySelector("#img").src = "https://api.qrserver.com/v1/create-qr-code/?size=240x240&data=" + QRtext;
+    }
+}
